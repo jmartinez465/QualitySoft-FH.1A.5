5c12c66a85221700b1bf877471ec74704d3835d3
Quang Phan <linkfarius.pnq@gmail.com>
2017-03-07 11:16:28 -0800

    test test

--- skeleton/TestProgram.java
+++ skeleton/TestProgram.java
@@ -1,6 +1,6 @@
 /**
  * Skeleton for main
- * tests Class Person, StreetAddress and ContactList //test
+ * tests Class Person, StreetAddress and ContactList
  * 
  * @version Draft-1 Mar 2 2017
  * @author Quang Phan
